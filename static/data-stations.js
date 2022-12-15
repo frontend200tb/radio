b8bea5b5f5602ef60e5e31e4186c65f837f0dcb8
Иван Каширин <85587225+frontend200tb@users.noreply.github.com>
2022-12-15 20:49:59 +0300

    Add files via upload

---
+++ static/data-stations.js
@@ -0,0 +1,331 @@
+export const dataStations = [
+  {
+    "title" : "Новое радио",
+    "country" : "Russia",
+    "stream" : "https://emgspb.hostingradio.ru:80/novoespb64.mp3",
+    "logo" : "rus-novoe.jpg",
+    "site" : "https://newradio.ru/"
+  },
+  {
+    "title" : "Хорошее радио",
+    "country" : "Russia",
+    "stream" : "https://hr.amgradio.ru/Horoshee",
+    "logo" : "rus-horoshee.svg",
+    "site" : "https://horoshee-radio.ru/"
+  },
+  {
+    "title" : "Русская волна",
+    "country" : "Russia",
+    "stream" : "https://ru1.amgradio.ru/RuWave48",
+    "logo" : "rus-russkaja-volna.svg",
+    "site" : "https://horoshee-radio.ru/"
+  },
+  {
+    "title" : "Сказка FM",
+    "country" : "Russia",
+    "stream" : "https://skazka.amgradio.ru/Skazka",
+    "logo" : "rus-skazka.png",
+    "site" : "https://horoshee-radio.ru/"
+  },
+  {
+    "title" : "Умное радио",
+    "country" : "Russia",
+    "stream" : "https://umnoe.amgradio.ru/Umnoe",
+    "logo" : "rus-umnoe.png",
+    "site" : "https://horoshee-radio.ru/"
+  },
+  {
+    "title" : "1 канал",
+    "country" : "Belarus",
+    "stream" : "https://stream2.datacenter.by/1kanal",
+    "logo" : "bel-1.jpg",
+    "site" : "https://www.tvr.by/radio/1-kanal/"
+  },
+  {
+    "title" : "Сталіца",
+    "country" : "Belarus",
+    "stream" : "https://stream2.datacenter.by/stalica",
+    "logo" : "bel-stalica.jpg",
+    "site" : "https://www.radiostalica.by/"
+  },
+  {
+    "title" : "Культура",
+    "country" : "Belarus",
+    "stream" : "https://stream2.datacenter.by/kultura",
+    "logo" : "bel-kultura.png",
+    "site" : "https://radiokultura.by/"
+  },
+  {
+    "title" : "Радиус FM",
+    "country" : "Belarus",
+    "stream" : "https://stream2.datacenter.by/radiusfm_main",
+    "logo" : "bel-radius.png",
+    "site" : "http://www.radiusfm.by/"
+  },
+  {
+    "title" : "Prvi Radio Novi Sad",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prviradions?mp=/stream",
+    "logo" : "srb-prvi-novi-sad.png",
+    "site" : "https://prviradio.rs/radiochannel/prvi-radio-novi-sad/"
+  },
+  {
+    "title" : "Prvi Radio Subotica",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prviradiosu?mp=/stream",
+    "logo" : "srb-prvi-subotica.png",
+    "site" : "https://prviradio.rs/radiochannel/prvi-radio-subotica/"
+  },
+  {
+    "title" : "Prvi Radio Užice",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prviradioue?mp=/stream",
+    "logo" : "srb-prvi-uzice.png",
+    "site" : "https://prviradio.rs/radiochannel/prvi-radio-uzice/"
+  },
+  {
+    "title" : "Prvi Radio Kafanica",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prvikafanica?mp=/stream",
+    "logo" : "srb-prvi-kafanica.png",
+    "site" : "https://prviradio.rs/radiochannel/kafanica/"
+  },
+  {
+    "title" : "Prvi Radio 90-te",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prvi90te?mp=/stream",
+    "logo" : "srb-prvi-90te.png",
+    "site" : "https://prviradio.rs/radiochannel/90-te/"
+  },
+  {
+    "title" : "Prvi Radio Cafe",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prvicafe?mp=/stream",
+    "logo" : "srb-prvi-cafe.png",
+    "site" : "https://prviradio.rs/radiochannel/cafe/"
+  },
+  {
+    "title" : "Prvi Radio Trending",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prvitrending?mp=/stream",
+    "logo" : "srb-prvi-trending.png",
+    "site" : "https://prviradio.rs/radiochannel/trending/"
+  },
+  {
+    "title" : "Prvi Radio YU kolekcija",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/prviyukolekcija?mp=/stream",
+    "logo" : "srb-prvi-yu.png",
+    "site" : "https://prviradio.rs/radiochannel/yu/"
+  },
+  {
+    "title" : "Radio In Beograd FM 95.8 MHz",
+    "country" : "Srbija",
+    "stream" : "https://radioin-64ssl.streaming.rs:9212/;stream.mp3",
+    "logo" : "srb-in.png",
+    "site" : "https://www.radioinbeograd.rs/"
+  },
+  {
+    "title" : "Radio Aparat Beograd",
+    "country" : "Srbija",
+    "stream" : "https://ca3ssl.rcast.net/stream/60815/",
+    "logo" : "srb-aparat.png",
+    "site" : "https://www.radioaparat.com/"
+  },
+  {
+    "title" : "Radio Puls Grocka",
+    "country" : "Srbija",
+    "stream" : "https://stream.iradio.pro/proxy/radiopuls?mp=/stream",
+    "logo" : "srb-puls.jpg",
+    "site" : "https://www.radiopuls.rs/"
+  },
+  {
+    "title" : "Radio 012 Požarevac",
+    "country" : "Srbija",
+    "stream" : "https://pozarevac-bobanex.radioca.st/stream",
+    "logo" : "srb-012.jpg",
+    "site" : "https://radio-012.tk/"
+  },
+  {
+    "title" : "Radio AS FM Novi Sad",
+    "country" : "Srbija",
+    "stream" : "https://mastermedia.shoutca.st/proxy/radioasfm?mp=/stream",
+    "logo" : "srb-asfm.png",
+    "site" : "https://radioas.fm/"
+  },
+  {
+    "title" : "Radio Šljivovica Kaljevo",
+    "country" : "Srbija",
+    "stream" : "https://stream.iradio.pro/proxy/radiosljivovica?mp=/stream",
+    "logo" : "srb-sljivovica.png",
+    "site" : "https://www.radiosljivovica.net/"
+  },
+  {
+    "title" : "Radio Sputnjik Srbija",
+    "country" : "Srbija",
+    "stream" : "https://icecast-rian.cdnvideo.ru/voicesrb",
+    "logo" : "srb-sputnik.jpg",
+    "site" : "https://sputnikportal.rs/radio/"
+  },
+  {
+    "title" : "Radio 101 Beograd",
+    "country" : "Srbija",
+    "stream" : "https://radiossl.radio101.rs/mp3",
+    "logo" : "srb-101.png",
+    "site" : "http://radio101.rs/"
+  },
+  {
+    "title" : "Radio Morava - Jagodina",
+    "country" : "Srbija",
+    "stream" : "https://e3.radiomorava.rs/radio/8000/radiomorava128.mp3",
+    "logo" : "srb-morava.png",
+    "site" : "http://www.radiomorava.rs/"
+  },
+  {
+    "title" : "Resavski Radio - Veliki Popovic",
+    "country" : "Srbija",
+    "stream" : "https://e3.radiomorava.rs/radio/8010/resavskiradio128.mp3",
+    "logo" : "srb-resavski.jpg",
+    "site" : "http://www.radiomorava.rs/"
+  },
+  {
+    "title" : "Morava VIC Radio - Jagodina",
+    "country" : "Srbija",
+    "stream" : "https://e3.radiomorava.rs/radio/8020/vic.mp3",
+    "logo" : "srb-morava-vic.jpg",
+    "site" : "http://www.radiomorava.rs/"
+  },
+  {
+    "title" : "Morava KOLO Radio - Jagodina",
+    "country" : "Srbija",
+    "stream" : "https://e3.radiomorava.rs/radio/8040/kolo.mp3",
+    "logo" : "srb-morava-kolo.jpg",
+    "site" : "http://www.radiomorava.rs/"
+  },
+  {
+    "title" : "Morava ETNO Radio - Jagodina",
+    "country" : "Srbija",
+    "stream" : "https://e3.radiomorava.rs/radio/8050/etno.mp3",
+    "logo" : "srb-morava-etno.jpg",
+    "site" : "http://www.radiomorava.rs/"
+  },
+  {
+    "title" : "Morava POP ROK Radio - Jagodina",
+    "country" : "Srbija",
+    "stream" : "https://e3.radiomorava.rs/radio/8060/poprok.mp3",
+    "logo" : "srb-morava-pop-rok.jpg",
+    "site" : "http://www.radiomorava.rs/"
+  },
+  {
+    "title" : "Morava TOP 50 Radio - Jagodina",
+    "country" : "Srbija",
+    "stream" : "https://e3.radiomorava.rs/radio/8070/top50.mp3",
+    "logo" : "srb-morava-top-50.jpg",
+    "site" : "http://www.radiomorava.rs/"
+  },
+  {
+    "title" : "Radio 1 Čakovec",
+    "country" : "Hrvatska",
+    "stream" : "https://azuracast.novi-net.net/radio/8020/radio1.mp3",
+    "logo" : "hrv-1.png",
+    "site" : "https://radio1.hr/"
+  },
+  {
+    "title" : "Radio 105 Selnica",
+    "country" : "Hrvatska",
+    "stream" : "https://azuracast.novi-net.net/radio/8050/radio105.mp3",
+    "logo" : "hrv-105.png",
+    "site" : "https://www.radio105.hr/"
+  },
+  {
+    "title" : "Radio 8 Sarajevo FM 106,8 MHz",
+    "country" : "BiH",
+    "stream" : "https://radio8-bhcloud.radioca.st/1.mp3",
+    "logo" : "bih-8.png",
+    "site" : "https://radio8.ba/"
+  },
+  {
+    "title" : "Gup Shup 94.3 FM Assam",
+    "country" : "India",
+    "stream" : "https://cors.bitwize.com.lb/http://103.95.48.18:8080/;",
+    "logo" : "ind-gup-shup.png",
+    "site" : "https://radiogupshup.com/"
+  },
+  {
+    "title" : "Radio Mirchi 98.3FM",
+    "country" : "India",
+    "stream" : "https://stream-56.zeno.fm/20g8vnc5mp8uv",
+    "logo" : "ind-mirchi.jpg",
+    "site" : "https://mirchi.in/"
+  },
+  {
+    "title" : "Big FM 106.2",
+    "country" : "India",
+    "stream" : "https://funasia.streamguys1.com/live4",
+    "logo" : "ind-bigfm.jpg",
+    "site" : "https://radiosindia.com/bigfm.html"
+  },
+  {
+    "title" : "Radio City 91.1FM",
+    "country" : "India",
+    "stream" : "https://stream-64.zeno.fm/pxc55r5uyc9uv",
+    "logo" : "ind-city.jpg",
+    "site" : "https://radiosindia.com/radiocityhindi.html"
+  },
+  {
+    "title" : "Spice FM UK 98.8",
+    "country" : "India",
+    "stream" : "https://spice988fm.radioca.st/stream",
+    "logo" : "ind-spicefmuk.jpg",
+    "site" : "https://radiosindia.com/spicefmuk.html"
+  },
+  {
+    "title" : "Radio Caravan",
+    "country" : "India",
+    "stream" : "https://stream.voxx.pro/radio/8260/radio.mp3",
+    "logo" : "ind-caravan.jpg",
+    "site" : "https://radiosindia.com/radiocaravan.html"
+  },
+  {
+    "title" : "Radio Olive 106.3FM",
+    "country" : "India",
+    "stream" : "https://playerservices.streamtheworld.com/api/livestream-redirect/OLIVE1063.mp3",
+    "logo" : "ind-olive.jpg",
+    "site" : "https://radiosindia.com/radioolive.html"
+  },
+  {
+    "title" : "Radio Shruti",
+    "country" : "India",
+    "stream" : "https://rs.suricloud.com/radio/8000/radio.mp3",
+    "logo" : "ind-shruti.jpg",
+    "site" : "https://radiosindia.com/radioshruti.html"
+  },
+  {
+    "title" : "Radio Namkin",
+    "country" : "India",
+    "stream" : "https://stream.zeno.fm/yg1qhsbaqv5tv",
+    "logo" : "ind-namkin.jpg",
+    "site" : "https://radiosindia.com/radionamkin.html"
+  },
+  {
+    "title" : "Desi Bollywood",
+    "country" : "India",
+    "stream" : "https://www.desizoneradio.com/relay2",
+    "logo" : "ind-desibollywood.jpg",
+    "site" : "https://radiosindia.com/desibollywood.html"
+  },
+  {
+    "title" : "EBC Radio 1170AM",
+    "country" : "India",
+    "stream" : "https://ice7.securenetsystems.net/WWTR?",
+    "logo" : "ind-ebc.jpg",
+    "site" : "https://radiosindia.com/ebcradio.html"
+  },
+  {
+    "title" : "Radio Badal",
+    "country" : "India",
+    "stream" : "https://stream.zeno.fm/nhrtcxg09u8uv",
+    "logo" : "ind-badal.jpg",
+    "site" : "https://radiosindia.com/radiobadal.html"
+  }
+]
